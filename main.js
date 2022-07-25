cc73b4dfb1974e82fe73542552c2b6458f3dd503
EC2 Default User <ec2-user@ip-172-31-42-24.us-west-2.compute.internal>
2022-07-25 09:50:09 +0000

    演算子の連続入力の部分をシンプルにしました

--- main.js
+++ main.js
@@ -8,41 +8,11 @@ function clickbutton(target){
         result.innerHTML = eval(result.innerHTML)
     } else {
         let last_value = result.innerHTML.slice(-1);
-        
-         if (target_value == "+"  &&last_value == "+" ){
-      result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "+"  &&last_value == "-" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else  if (target_value == "+"  &&last_value == "*" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "+"  &&last_value == "/" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "-"  &&last_value == "+" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "-"  &&last_value == "-" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "-"  &&last_value == "*" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "-"  &&last_value == "/" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "*"  &&last_value == "+" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "*"  &&last_value == "-" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "*"  &&last_value == "*" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "*"  &&last_value == "/" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "/"  &&last_value == "+" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "/"  &&last_value == "-" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "/"  &&last_value == "*" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         } else if (target_value == "/"  &&last_value == "/" )　{
-            result.innerHTML = result.innerHTML.slice(0, -1) + target_value
-         }
-         
+       if ((last_value=="+" || last_value=="-" || last_value=="*" || last_value=="/")&&
+             (target_value=="+" || target_value=="-" || target_value=="*" || target_value=="/" )){
+              result.innerHTML = result.innerHTML.slice(0, -1) + target_value
+           
+             }      
              else  if (result.innerHTML == "0") {
               result.innerHTML = target_value
          } else {
